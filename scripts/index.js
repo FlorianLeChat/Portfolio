7b18527d5d3ba56c165d916b8cd94bbd69726ddf
FlorianLeChat <floriantrayon942@gmail.com>
2021-12-14 21:26:54 +0100

    Added a dynamic age calculator to the about me section

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,15 @@ for ( const project of projects.values() )
 		}
 	} );
 }
+
+//
+// Permet de mettre à jour dynamiquement l'âge indiqué dans la
+// 	section "à propos de moi" (si ça ce n'est pas du dynamisme !).
+//
+const now = new Date().getTime();								// Horodatage de l'heure actuelle
+const born = Date.parse( "08 Aug 1999 00:00:00 GMT" );			// Horodatage de ma date de naissance
+const relative = new Date( now - born ).getFullYear() - 1970;	// Nombre d'années séparant les deux dates
+
+const aboutme = document.querySelector( "#aboutme > p" );
+
+aboutme.innerHTML = aboutme.innerHTML.replace( /[0-9]+/g, relative );
