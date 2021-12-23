c5c2c15d3f2f40cbbd80700fa3637d6089db7bd1
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-12-23 15:59:29 +0100

    Split all files for PHP addition

---
+++ include/views/2_header.php
@@ -0,0 +1,42 @@
+<?php
+	// Ceci est le fichier générique de l'en-tête du document
+
+	// -> requête traduction
+	// -> check if $file existe
+?>
+
+<!-- Avertissement page sans JavaScript -->
+<noscript>Votre navigateur ne supporte pas ou refuse de charger le JavaScript.</noscript>
+
+<!-- En-tête de la page -->
+<header>
+	<!-- Prénom & Nom -->
+	<h1>Florian Trayon</h1>
+
+	<!-- Description succincte -->
+	<h2>Développeur Web (Front/Back-End)</h2>
+
+	<!-- Plateformes de communication -->
+	<a href="https://discord.com/users/183272411167326209" title="Discord" target="_blank">
+		<img data-color="#5865f2" src="images/platforms/discord.svg" width="32" height="32" draggable="false" alt="Logo de Discord" />
+	</a>
+
+	<a href="https://github.com/FlorianLeChat" title="GitHub" target="_blank">
+		<img data-color="#27272a" src="images/platforms/github.svg" width="32" height="32" draggable="false" alt="Logo de GitHub" />
+	</a>
+
+	<a href="https://twitter.com/FlorianTrayon" title="Twitter" target="_blank">
+		<img data-color="#1da1f2" src="images/platforms/twitter.svg" width="32" height="32" draggable="false" alt="Logo de Twitter" />
+	</a>
+
+	<a href="https://www.linkedin.com/in/florian-trayon-826841227/" title="LinkedIn" target="_blank">
+		<img data-color="#006699" src="images/platforms/linkedin.svg" width="32" height="32" draggable="false" alt="Logo de LinkedIn" />
+	</a>
+
+	<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" title="Curriculum Vitae" target="_blank">
+		<img data-color="#ffffff" src="images/platforms/cv.svg" width="32" height="32" draggable="false" alt="Logo représentant le C.V" />
+	</a>
+
+	<!-- Vagues de fin -->
+	<img src="images/decorations/header_waves_blue.svg" alt="" />
+</header>
---
+++ include/views/6_footer.php
@@ -0,0 +1,13 @@
+<?php
+	// Ceci est le fichier générique du pied-de-page du site
+
+	// -> loop à travers les données
+?>
+
+<footer>
+	<ul>
+		<li><a href="?thanks=1">Contributions</a></li>
+		<li><a href="admin/index.php">Administration</a></li>
+		<li><a href="https://github.com/FlorianLeChat/Portfolio">Code source</a></li>
+	</ul>
+</footer>
