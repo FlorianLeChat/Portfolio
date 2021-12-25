631010263c43b90396e3d260cf5b949f9181c70a
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-12-25 15:22:32 +0100

    Added a kind of dynamism to the index page

--- include/views/index.php
+++ include/views/index.php
@@ -1,27 +1,78 @@
 <?php
-	// Ceci est le fichier de la page d'accueil du site
+	//
+	// Ceci est le fichier permettant de contrôler la vue de la page d'accueil du site.
+	//
+
+	// On récupère les données de la section "à propos de moi".
+	//	Note : on remplace les sauts de ligne SQL par leur équivalent en HTML (\n => <br />)
+	$about_me = $translation->getPhrases("about_me");
+	$about_me_description = str_replace("\r\n\r\n", "<br /><br />\n\t\t", $about_me["about_me_description"]);
+
+	//
+	$projects_title = $translation->getPhrase("projects_title");
+
+	//
+	$skills = $translation->getPhrases("skills");
+
+	//
+	$contact = $translation->getPhrases("contact");
+
+	// ------------------------
+	// PROJETS
+	// ------------------------
+	include_once("./include/controllers/database.php");
+
+	use Portfolio\Controllers\Connector;
+
+	$connector = new Connector();
+	$connector = $connector->getPDO();
+
+	$t1 = $connector->prepare("SELECT `identifier`, `background_image` FROM `projects` ORDER BY `projects`.`position` ASC;");
+	$t1->execute();
+	$t1 = $t1->fetchAll();
+
+	array_unshift($t1, "");
+	unset($t1[0]);
+
+	$projects = "<div class=\"row\">\n";
+
+	foreach ($t1 as $key => $value)
+	{
+		$image = $value["background_image"];
+		$identifier = $value["identifier"];
+
+		$name = $translation->getPhrase("project_" . $identifier . "_title");
+
+		$projects .= <<<ARTICLE
+			\t\t<!-- Project numéro $key -->
+			\t\t<article data-image="images/projects/$image">
+				\t\t<h3><a href="?target=projects&name=$identifier">$name</a></h3>
+			\t\t</article>\n
+		ARTICLE;
+
+		if ($key > 0 && $key % 3 == 0)
+		{
+			$projects .= "\t\t</div>\n";
+
+			if ($key != count($t1))
+			{
+				$projects .= "\t\t<div class=\"row\">\n";
+			}
+		}
+	}
+	// --------------------------
 ?>
 
 <!-- À propos de moi -->
 <section id="aboutme">
-	<h3>#informations</h3>
+	<h3>#aboutme</h3>
 
-	<h2>À propos de moi</h2>
+	<h2>
+		<?php echo($about_me["about_me_title"] . "\n"); ?>
+	</h2>
 
 	<p>
-		Bienvenue sur mon portfolio !
-		<br /><br />
-		Je suis Florian, un développeur français de 22 ans.
-		Je suis principalement un développeur <em>back-end</em> (NodeJS, PHP, ...) mais je suis
-		vraiment intéressé par l'apprentissage du <em>front-end</em> (HTML, CSS, JavaScript, ...)
-		et du design UI/UX.
-		<br /><br />
-		J'ai également de l'expérience dans les langages de script et de programmation comme le Lua,
-		le Python et le C# (avec .NET Core). Cela inclut évidemment des connaissances solides dans
-		les systèmes de sauvegarde SQLite et MySQL utilisés également dans le Web.
-		<br /><br />
-		Vous pourrez en apprendre plus sur moi, mes expériences, mes projets et mes compétences grâce
-		à ce Portfolio.
+		<?php echo($about_me["about_me_description"] . "\n"); ?>
 	</p>
 </section>
 
@@ -33,36 +84,14 @@
 	<!-- Listes des projets -->
 	<h3>#projects</h3>
 
-	<h2>&#8250; <a href="?target=projects">Projets</a> &#8249;</h2>
+	<h2>
+		&#8250; <a href="?target=projects"><?php echo($projects_title); ?></a> &#8249;
+	</h2>
 
 	<div class="container">
-		<div class="row">
-			<article data-image="images/projects/placeholder.jpg">
-				<h3><a href="?target=projects&name=raven">Raven Framework</a></h3>
-			</article>
-
-			<article data-image="images/projects/placeholder.jpg">
-				<h3><a href="?target=projects&name=discordbot">Florian's Bot</a></h3>
-			</article>
-
-			<article data-image="images/projects/placeholder.jpg">
-				<h3><a href="?target=projects&name=facepunch">Facepunch Commits Monitor</a></h3>
-			</article>
-		</div>
-
-		<div class="row">
-			<article data-image="images/projects/placeholder.jpg">
-				<h3><a href="?target=projects&name=phpstorage">Simple File Storage</a></h3>
-			</article>
-
-			<article data-image="images/projects/placeholder.jpg">
-				<h3><a href="?target=projects&name=steam">Steam Collection Download Size Calculator</a></h3>
-			</article>
-
-			<article data-image="images/projects/placeholder.jpg">
-				<h3><a href="?target=projects&name=python">Python RPG</a></h3>
-			</article>
-		</div>
+		<?php
+			echo($projects);
+		?>
 	</div>
 
 	<!-- Vagues de fin -->
@@ -73,30 +102,32 @@
 <section id="skills">
 	<h3>#skills</h3>
 
-	<h2>&#8250; <a href="?target=skills">Compétences</a> &#8249;</h2>
+	<h2>
+		&#8250; <a href="?target=projects"><?php echo($skills["skills_title"]); ?></a> &#8249;
+	</h2>
 
 	<div>
 		<article>
 			<img src="images/skills/school.svg" draggable="false" width="64" height="64" alt="Image représentant une école." />
 
-			<h2>Parcours scolaire & diplômes</h2>
+			<h2>
+				<?php echo($skills["skills_school_title"] . "\n"); ?>
+			</h2>
 
 			<p>
-				<strong><a href="?target=skills&name=school">Cliquez ici</a></strong> pour en savoir plus sur
-				mon parcours scolaire ainsi que les diplômes et certificats que j'ai pu obtenir
-				jusqu'à aujourd'hui.
+				<?php echo($skills["skills_school_description"] . "\n"); ?>
 			</p>
 		</article>
 
 		<article>
 			<img src="images/skills/work.svg" draggable="false" width="64" height="64" alt="Image représentant le travail." />
 
-			<h2>Compétences professionnelles</h2>
+			<h2>
+				<?php echo($skills["skills_work_title"] . "\n"); ?>
+			</h2>
 
 			<p>
-				<strong><a href="?target=skills&name=work">Cliquez ici</a></strong> pour en savoir plus sur mes
-				compétences acquises durant mon apprentissage en autodidacte mais aussi lors de
-				stages en milieu professionnel.
+				<?php echo($skills["skills_work_description"] . "\n"); ?>
 			</p>
 		</article>
 	</div>
@@ -106,9 +137,15 @@
 <section id="contact">
 	<h3>#contact</h3>
 
-	<h2>&#8250; <a href="?target=contact">Contact</a> &#8249;</h2>
+	<h2>
+		&#8250; <a href="?target=projects"><?php echo($contact["contact_title"]); ?></a> &#8249;
+	</h2>
 
-	<h2>Un projet ? Une demande ?<br /><br />Contactez-moi !</h2>
+	<h2>
+		<?php echo($contact["contact_description"] . "\n"); ?>
+	</h2>
 
-	<span><a href="?target=contact">Formulaire de contact</a></span>
+	<span>
+		<a href="?target=contact"><?php echo($contact["contact_button"]); ?></a>
+	</span>
 </section>
