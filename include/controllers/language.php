c6bbb2f126692d384889c420f0a4914c6d2f9e69
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-12-25 18:19:28 +0100

    Removed unnecessary SQL constructor in the language controller

--- include/controllers/language.php
+++ include/controllers/language.php
@@ -5,24 +5,12 @@
 	namespace Portfolio\Controllers;
 
 	include_once("./include/models/language.php");
-	include_once("./include/controllers/database.php");
 
 	use Portfolio\Models\Language;
-	use Portfolio\Controllers\Connector;
 
 	// Classe permettant de manipuler les traductions.
 	class Translation extends Language
 	{
-		//
-		// Permet d'initialiser la connexion avec la base de données.
-		//
-		public function __construct()
-		{
-			// On instancie le connecteur SQL.
-			$this->connector = new Connector();
-			$this->connector = $this->connector->getPDO();
-		}
-
 		//
 		// Permet de vérifier si une langue existe au niveau de la base de données.
 		//
