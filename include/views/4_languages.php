e024b79afa90d490ebcf59a97695c8f742942447
Florian Trayon <git@florian-trayon.fr>
2021-12-26 17:53:51 +0100

    Improved the language selector

--- include/views/4_languages.php
+++ include/views/4_languages.php
@@ -7,27 +7,41 @@
 
 <aside>
 	<!-- Sélection de la langue -->
-	<ul id="flags">
-		<li>
-			<img src="images/flags/fr.svg" width="21" height="16" draggable="false" alt="Drapeau de langue française" />
-			<p><a href="?lang=fr">Français</a></p>
-		</li>
+	<form method="POST" action="?target=<?php echo($file); ?>">
+		<ul id="flags">
+			<li>
+				<!-- Français -->
+				<button type="submit" name="language" value="fr">
+					<img src="images/flags/fr.svg" width="21" height="16" draggable="false" alt="Drapeau de la langue française" />
+					<span>Français</span>
+				</button>
+			</li>
 
-		<li>
-			<img src="images/flags/gb.svg" width="21" height="16" draggable="false" alt="Drapeau de la langue anglaise" />
-			<p><a href="?lang=en">Anglais</a></p>
-		</li>
+			<li>
+				<!-- Anglais -->
+				<button type="submit" name="language" value="en">
+					<img src="images/flags/gb.svg" width="21" height="16" draggable="false" alt="Drapeau de la langue anglaise" />
+					<span>Anglais</span>
+				</button>
+			</li>
 
-		<li>
-			<img src="images/flags/es.svg" width="21" height="16" draggable="false" alt="Drapeau de la langue espagnol" />
-			<p><a href="?lang=es">Espagnol</a></p>
-		</li>
+			<li>
+				<!-- Espagnol -->
+				<button type="submit" name="language" value="es">
+					<img src="images/flags/es.svg" width="21" height="16" draggable="false" alt="Drapeau de la langue espagnol" />
+					<span>Anglais</span>
+				</button>
+			</li>
 
-		<li>
-			<img src="images/flags/jp.svg" width="21" height="16" draggable="false" alt="Drapeau du la langue japonaise" />
-			<p><a href="?lang=jp">Japonais</a></p>
-		</li>
-	</ul>
+			<li>
+				<!-- Japonais -->
+				<button type="submit" name="language" value="jp">
+					<img src="images/flags/jp.svg" width="21" height="16" draggable="false" alt="Drapeau de la langue japonaise" />
+					<span>Japonais</span>
+				</button>
+			</li>
+		</ul>
+	</form>
 
 	<!-- Retour au début de la page -->
 	<button id="scrollTop" title="Retour au début de la page"></button>
