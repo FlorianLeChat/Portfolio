8213c28ee9ceee3870c74c1cedabbb1588c9b7d9
Florian Trayon <git@florian-trayon.fr>
2021-12-26 14:03:52 +0100

    Added a PHP script to control the contact header

--- include/views/2_header_contact.php
+++ include/views/2_header_contact.php
@@ -1,17 +1,20 @@
 <?php
-	// Ceci est le fichier générique de l'en-tête du document
+	//
+	// Ceci est le fichier permettant de contrôler la vue de l'en-tête de la page de contact.
+	//
 
-	// -> requête traduction
-	// -> check if $file existe
+	// On récupère les deux traductions pour le titre et le sous-titre
+	//	de la page.
+	$header_contact = $translation->getPhrases("header_contact");
 ?>
 
 <!-- En-tête de la page -->
 <header>
 	<!-- Titre de la catégorie -->
-	<h1>Mes moyens de contact</h1>
+	<h1><?php echo($header_contact["header_contact_title"]); ?></h1>
 
 	<!-- Description succincte -->
-	<h2>Les moyens permettant de me contacter directement</h2>
+	<h2><?php echo($header_contact["header_contact_subtitle"]); ?></h2>
 
 	<!-- Vagues de fin -->
 	<img src="images/decorations/header_waves_purple.svg" alt="" />
