1abd4018f1f057ec852e3c85244867f8fdd27e0f
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-12-26 13:54:19 +0100

    Added a PHP script to control the projects header

--- include/views/2_header_projects.php
+++ include/views/2_header_projects.php
@@ -1,17 +1,20 @@
 <?php
-	// Ceci est le fichier générique de l'en-tête du document
+	//
+	// Ceci est le fichier permettant de contrôler la vue de l'en-tête de la page des projets.
+	//
 
-	// -> requête traduction
-	// -> check if $file existe
+	// On récupère les deux traductions pour le titre et le sous-titre
+	//	de la page.
+	$header_projects = $translation->getPhrases("header_projects");
 ?>
 
 <!-- En-tête de la page -->
 <header>
 	<!-- Titre de la catégorie -->
-	<h1>Mes projets</h1>
+	<h1><?php echo($header_projects["header_projects_title"]); ?></h1>
 
 	<!-- Description succincte -->
-	<h2>Mes réalisations personnelles les plus abouties</h2>
+	<h2><?php echo($header_projects["header_projects_subtitle"]); ?></h2>
 
 	<!-- Vagues de fin -->
 	<img src="images/decorations/header_waves_orange.svg" alt="" />
