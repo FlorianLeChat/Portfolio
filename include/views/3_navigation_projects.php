58ab3be297ca6e95b6c4f7a01fec3bc52a67cf56
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-12-26 19:09:44 +0100

    Fixed the navigation bar by separating the code into several files

---
+++ include/views/3_navigation_projects.php
@@ -0,0 +1,18 @@
+<?php
+	//
+	// Ceci est le fichier permettant de contrôler la vue de la barre de navigation (page des projets).
+	//
+
+	// On récupère les traductions des titres dans la base de données.
+	$about_me_title = $translation->getPhrase("about_me_title");			// À propos de moi
+	$header_skills_title = $translation->getPhrase("header_skills_title");	// Compétences
+	$contact_title = $translation->getPhrase("contact_title");				// Contact
+?>
+
+<nav>
+	<ul>
+		<li><a href="?target=index"><?php echo($about_me_title); ?></a></li>
+		<li><a href="?target=skills"><?php echo($header_skills_title); ?></a></li>
+		<li><a href="?target=contact"><?php echo($contact_title); ?></a></li>
+	</ul>
+</nav>
