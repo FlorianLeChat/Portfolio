4a8e26867b3eb14d9b349985060b6f49c283bfab
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2021-12-28 20:35:54 +0100

    Fixed files inclusion was called outside of the document root

--- include/views/1_head.php
+++ include/views/1_head.php
@@ -5,13 +5,24 @@
 
 	// On récupère les traductions depuis la base de données.
 	$head = $translation->getPhrases("head");
+
+	// On détermine le chemin d'accès vers les feuilles de style
+	//	CSS et fichiers JavaScript.
+	//	Note : cette vérification est faite pour inclure ce fichier
+	//		dans la page d'administration.
+	$path = "";
+
+	if (str_contains($_SERVER["SCRIPT_NAME"], "admin"))
+	{
+		$path = "../$path";
+	}
 ?>
 
 <head>
 	<!-- Méta-données du document -->
 	<meta charset="utf-8" />
 	<meta name="Author" content="Florian Trayon" />
-	<meta name="description" content="<?php echo($head["head_description"]); ?>"/>
+	<meta name="description" content="<?php echo($head["head_description"]); ?>" />
 	<meta name="keywords" lang="<?php echo($language); ?>" content="portfolio, web, html, css, frontend, backend, fullstack" />
 	<meta name="robots" content="noindex" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -27,13 +38,13 @@
 	<!-- Polices & feuilles de style CSS -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" crossorigin />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" crossorigin />
-	<link rel="stylesheet" href="styles/main.css" />
-	<link rel="stylesheet" href="styles/<?php echo($file); ?>.css" />
+	<link rel="stylesheet" href="<?php echo($path . "styles/main.css"); ?>" />
+	<link rel="stylesheet" href="<?php echo($path . "styles/$file.css"); ?>" />
 
 	<!-- Scripts JavaScript -->
-	<script src="scripts/main.js" defer></script>
-	<script src="scripts/<?php echo($file); ?>.js" defer></script>
+	<script src="<?php echo($path . "scripts/main.js"); ?>" defer></script>
+	<script src="<?php echo($path . "scripts/$file.js"); ?>" defer></script>
 
 	<!-- Icône du document (16x16) -->
-	<link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+	<link rel="icon" href="<?php echo($path . "images/favicon.ico"); ?>" type="image/x-icon" />
 </head>
