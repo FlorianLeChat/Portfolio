9a671289f18f5a40b1c70f44f8fd5ed2e71ac0b8
FlorianLeChat <floriantrayon942@gmail.com>
2022-01-09 16:36:42 +0100

    Improved the image gallery of the projects page

--- include/views/projects.php
+++ include/views/projects.php
@@ -32,14 +32,14 @@
 		$files = array_values($files);					// Réarrangement des indices du tableau.
 	}
 
-	foreach ($projects_data as $key => $value)
+	foreach ($projects_data as $project)
 	{
 		// On construit le titre du projet.
 		// Note : durant cette étape, il est nécessaire de vérifier
 		//	si le projet est « open source », dans ce cas, un lien
 		//	clickable doit être ajouté.
-		$url = $value["source_url"];
-		$identifier = $value["identifier"];
+		$url = $project["source_url"];
+		$identifier = $project["identifier"];
 
 		$name = $projects["project_" . $identifier . "_title"];
 		$title = $name;
@@ -53,18 +53,20 @@
 		// On construit ensuite les images présentes dans la galerie
 		//	des photos (uniquement si le répertoire existe).
 		$images = "\n";
+		$indice = 0;
 
 		if (is_dir($path))
 		{
-			foreach ($files as $key => $image)
+			foreach ($files as $image)
 			{
 				// On vérifie si le chemin d'accès de l'image semble
 				//	appartenir au projet actuellement à l'étape de la boucle.
-				if (str_contains($image, $identifier))
+				if (str_contains($image, $identifier . "_gallery"))
 				{
 					// À ce niveau, le chemin d'accès doit comporter le nom
 					//	de l'image récupéré précédemment.
 					$image = $path . $image;
+					$indice++;
 
 					// On tente de lire l'en-tête des images de la galerie des photos
 					//	pour récupérer les commentaires insérées au préalable.
@@ -72,9 +74,10 @@
 
 					if (array_key_exists("Comments", $header))
 					{
-						// Le champ des commentaires existe, on réalise une conversion
+						// Le champ des commentaires existe, on réalise des conversions
 						//	du jeu de caractères pour obtenir une description lisible.
 						$label = mb_convert_encoding($header["Comments"], "byte2le");
+						$label = mb_convert_encoding($label, "HTML-ENTITIES", "byte2be");
 					}
 					else
 					{
@@ -86,8 +89,8 @@
 					//	requise pour la disposition en CSS.
 					$images .= <<<IMAGE
 						\t\t<div>
-							\t\t<a href="$image">
-								\t\t<img src="$image" draggable="false" alt="Image - $key" />
+							\t\t<a href="$image" target="_blank">
+								\t\t<img src="$image" draggable="false" alt="Image - $indice" />
 							\t\t</a>
 
 							\t\t<p>$label</p>
@@ -100,7 +103,7 @@
 		// On construit après les images qui représentent les logos
 		//	de programmation utilisés par les projets.
 		$logos = "";
-		$languages = json_decode($value["languages"]);
+		$languages = json_decode($project["languages"]);
 
 		foreach ($languages as $language)
 		{
@@ -110,7 +113,7 @@
 		// On assemble enfin toutes les parties créées précédemment
 		// 	pour faire une succession d'articles qui décrivent chacun
 		//	les projets de manière détaillée.
-		$date = $value["creation_date"];
+		$date = $project["creation_date"];
 		$description = $projects["project_" . $identifier . "_description"];
 
 		$projects_html .= <<<ARTICLE
@@ -134,7 +137,9 @@
 
 				<!-- Galerie photos -->
 				<div class="images">
+					<img src="images/decorations/arrow_left.svg" alt="" />
 					$images
+					<img src="images/decorations/arrow_right.svg" alt="" />
 				</div>
 
 				<hr />
