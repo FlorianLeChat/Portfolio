6de96b2ee9a0dee904e95e667bee53035253b455
Florian Trayon <git@florian-trayon.fr>
2022-02-12 10:39:46 +0100

    Fixed background images caching issues on the skills page

--- scripts/skills.js
+++ scripts/skills.js
@@ -1,27 +1,15 @@
 //
-// Permet de contrôler le mécanisme d'affichage/disparition de l'image
-//	en arrière plan au survol de la souris.
+// Permet d'ajouter les images d'arrière-plan des lieux où s'est
+//	déroulé la formation.
 //
 const courses = document.querySelectorAll( "#list li" );
 
 for ( const course of courses.values() )
 {
-	// On ajoute un événement qui se déclenchera lorsque la souris
-	//	est en train de survoler l'élément.
-	course.addEventListener( "mouseenter", ( _event ) =>
-	{
-		const target = course.getAttribute( "data-image" );					// Image ciblée
-		const current = window.getComputedStyle( course, ).backgroundImage;	// Image de secours
+	const target = course.getAttribute( "data-image" );					// Image ciblée
+	const current = window.getComputedStyle( course ).backgroundImage;	// Image de secours
 
-		course.style.backgroundImage = `url( ${ target } ), ${ current }`;
-	} );
-
-	// On supprime l'image lorsque la souris n'est plus dans la zone de
-	// 	sélection de l'élément.
-	course.addEventListener( "mouseleave", ( _event ) =>
-	{
-		course.removeAttribute( "style" );
-	} );
+	course.style.backgroundImage = `url( ${ target } ), ${ current }`;
 }
 
 //
