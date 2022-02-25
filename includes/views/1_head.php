b61744664f90e1aaa3767f378f1a7898c01deaa2
Florian Trayon <git@florian-trayon.fr>
2022-02-25 09:38:51 +0100

    Removed robots access prevention

--- includes/views/1_head.php
+++ includes/views/1_head.php
@@ -24,7 +24,6 @@
 	<meta name="Author" content="Florian Trayon" />
 	<meta name="description" content="<?php echo($head["head_description"]); ?>" />
 	<meta name="keywords" lang="<?php echo($language); ?>" content="portfolio, web, html, css, frontend, backend, fullstack" />
-	<meta name="robots" content="noindex" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 	<!-- Titre du document -->
