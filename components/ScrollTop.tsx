806522655d321147eb341dd90ad3ede27d5718c0
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-02-11 18:10:38 +0100

    Added button component to return to the top of the page

---
+++ components/ScrollTop.tsx
@@ -0,0 +1,14 @@
+//
+// Composant permettant de remonter en haut de page.
+//
+export default function ScrollTop()
+{
+	// Affichage du rendu HTML du composant.
+	return (
+		<aside>
+			<a href="#">
+				<i className="fas fa-arrow-up"></i>
+			</a>
+		</aside>
+	);
+}
