af8a6b57452f519f84c533ca083e41e71974afd5
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-02-11 16:23:01 +0100

    Added additional attributes to the <html> tag of the website

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,17 @@
+//
+// Page utilisée pour générer le contenu dynamique HTML de la page.
+// 	Source : https://nextjs.org/docs/advanced-features/custom-document
+//
+
+// Importation des dépendances.
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document()
 {
 	return (
-		<Html lang="en">
 			<Head />
 			<body>
+		<Html className="theme--bright" lang="en" dir="auto" prefix="og: https://ogp.me/ns#">
 				<Main />
 				<NextScript />
 			</body>
