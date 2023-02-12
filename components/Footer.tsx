275e2b7373ed75f4e1e716601b4e8c3792957145
Florian Trayon <git@florian-trayon.fr>
2023-02-12 18:41:26 +0100

    Added website creation date and a link to its author in the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,16 @@
 //
 // Composant du pied de page générique du site.
 //
-import Link from "next/link";
-
 export default function Footer()
 {
 	// Affichage du rendu HTML du composant.
 	return (
 		<footer>
 			{/* Lien vers le dépôt GitHub du projet */}
-			<strong>Réalisé par <Link href="https://github.com/FlorianLeChat/Portfolio">Florian Trayon 🐈</Link> !</strong>
+			<span>Réalisé par <a href="https://github.com/FlorianLeChat">Florian Trayon 🐈</a></span>
+
+			{/* Date de création du site */}
+			<small>&copy; {new Date().getFullYear()}</small>
 		</footer>
 	);
 }
