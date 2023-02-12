5063341b63a3b4d2bda2bece5a431090057e2c87
Florian Trayon <git@florian-trayon.fr>
2023-02-12 15:03:16 +0100

    Fixed return to top of the page mechanism

--- components/ScrollTop.tsx
+++ components/ScrollTop.tsx
@@ -3,15 +3,43 @@
 //
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 
 export default function ScrollTop()
 {
-	// Affichage du rendu HTML du composant.
-	return (
-		<aside>
-			<a href="#">
-				<FontAwesomeIcon icon={faArrowUp} />
-			</a>
-		</aside>
-	);
+	// Déclaration des variables d'état.
+	const [ showButton, setShowButton ] = useState( false );
+
+	// Affichage ou disparition du bouton de remontée en haut de page.
+	const onScroll = () =>
+	{
+		setShowButton( window.scrollY > 200 );
+	};
+
+	// Détection du défilement de la page.
+	//	Source : https://github.com/vercel/next.js/issues/6132#issuecomment-790623507
+	useEffect( () =>
+	{
+		window.addEventListener( "scroll", onScroll );
+
+		return () => window.removeEventListener( "scroll", onScroll );
+	} );
+
+	// Affichage conditionnel du rendu HTML du composant.
+	if ( showButton )
+	{
+		// L'utilisateur se trouve à plus de 200 pixels du haut de la page.
+		return (
+			<aside>
+				<a href="#">
+					<FontAwesomeIcon icon={faArrowUp} />
+				</a>
+			</aside>
+		);
+	}
+	else
+	{
+		// L'utilisateur se trouve à moins de 200 pixels du haut de la page.
+		return <></>;
+	}
 }
