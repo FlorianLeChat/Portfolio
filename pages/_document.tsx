a9c0374c0e9e0e30e2e6e8fbbb1f60de9c1d4cce
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-02-14 11:01:26 +0100

    Added Google Analytics services

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,33 @@
 //
 
 // Importation des dépendances.
-import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+import { Html, Main, Head, NextScript } from "next/document";
 
 export default function Document()
 {
 	// Affichage du rendu HTML de la page.
 	return (
 		<Html lang="en" dir="auto" prefix="og: https://ogp.me/ns#">
-			<Head />
+			<Head>
+				{/* Scripts JavaScript */}
+				<Script src={`https://www.googletagmanager.com/gtag/js?id=${ process.env[ "NEXT_PUBLIC_ANALYTICS_IDENTIFIER" ] }`} strategy="afterInteractive" />
+
+				{/* Google Analytics */}
+				<Script strategy="afterInteractive">
+					{`
+						window.dataLayer = window.dataLayer || [];
+
+						function gtag()
+						{
+							dataLayer.push( arguments );
+						}
+
+						gtag( "js", new Date() );
+						gtag( "config", "${ process.env[ "NEXT_PUBLIC_ANALYTICS_IDENTIFIER" ] ?? "" }" );
+					`}
+				</Script>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
