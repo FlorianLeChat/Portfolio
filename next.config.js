295fdb8ca7eb8cc2ec94ebe7c6f47d833250f80d
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-02-17 18:04:18 +0100

    Added redirects and internal settings for NextJS

--- next.config.js
+++ next.config.js
@@ -4,5 +4,18 @@
  * @type {import("next").NextConfig}
  */
 module.exports = {
-	reactStrictMode: true
+	basePath: process.env[ "NEXT_PUBLIC_BASE_PATH" ],
+	poweredByHeader: false,
+	reactStrictMode: true,
+	async redirects()
+	{
+		return [
+			{
+				// Redirection vers le dépôt GitHub.
+				source: "/source",
+				permanent: true,
+				destination: "https://github.com/FlorianLeChat/Portfolio"
+			}
+		];
+	}
 };
