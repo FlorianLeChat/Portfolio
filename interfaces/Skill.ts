5ce19ee74eb8d83bdaa97676c85edf3ea2da151e
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-02-18 18:08:40 +0100

    Added skills category filtering

---
+++ interfaces/Skill.ts
@@ -0,0 +1,14 @@
+//
+// Interface des attributs d'une compétence.
+//
+export interface SkillAttributes
+{
+	// Nom de la compétence.
+	name: string;
+
+	// Icône de la compétence.
+	icon: string;
+
+	// Type de la compétence.
+	type: string;
+}
