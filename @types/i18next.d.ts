6993190767257d254f96271e68b627b15ec1abae
Florian Trayon <git@florian-trayon.fr>
2023-03-23 18:46:58 +0100

    Fixed translation typing with earlier versions of `next-i18next`.

--- @types/i18next.d.ts
+++ @types/i18next.d.ts
@@ -6,6 +6,9 @@ declare module "i18next"
 	interface CustomTypeOptions
 	{
 		defaultNS: "common";
+		resources: {
+			common: typeof import( "@/public/locales/en/common.json" );
+		};
 		returnNull: false;
 		returnEmptyString: false;
 	}
