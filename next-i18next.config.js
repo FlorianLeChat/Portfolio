bcc06621da6a62ffe992d4c2f03eccfa0e94187a
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-04-09 13:52:24 +0200

    Removed i18n debug messages in dev environment

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,7 +4,6 @@
  * @type {import("next-i18next").UserConfig}
  */
 module.exports = {
-	debug: process.env.NODE_ENV === "development",
 	reloadOnPrerender: process.env.NODE_ENV === "development",
 	localePath: typeof window === "undefined" ? require( "path" ).resolve( "./public/locales" ) : "/locales",
 	i18n: {
