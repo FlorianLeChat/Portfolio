176dfbe860cb7a1898755359f2096127544bb83a
Florian Trayon <git@florian-trayon.fr>
2023-04-29 17:42:47 +0200

    Added a modal window for cookies and trackers under GDPR

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,8 @@
 // Importation des dépendances.
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
-import type { ReactNode } from "react";
+import * as CookieConsent from "vanilla-cookieconsent";
+import { useEffect, ReactNode } from "react";
 
 // Importation des fonctions utilitaires.
 import { ThemeProvider } from "@/utils/ThemeContext";
@@ -21,10 +22,60 @@ export default function Layout( { children }: { children: ReactNode; } )
 	const router = useRouter();
 	const shouldHide = router.pathname === "/404" || router.pathname === "/500" || router.pathname === "/_offline";
 
+	// Affichage du consentement des cookies.
+	useEffect( () =>
+	{
+		CookieConsent.run(
+			{
+				// Désactivation de l'interaction avec la page.
+				disablePageInteraction: true,
+
+				// Paramètres internes des cookies.
+				cookie: {
+					path: router.basePath
+				},
+
+				// Paramètres de l'interface utilisateur.
+				guiOptions: {
+					consentModal: {
+						layout: "bar",
+						position: "bottom center"
+					}
+				},
+
+				// Configuration des catégories de cookies.
+				categories: {
+					necessary: {
+						enabled: true,
+						readOnly: true
+					},
+					analytics: {
+						autoClear: {
+							cookies: [
+								{
+									name: /^(_ga|_gid)/
+								}
+							]
+						}
+					}
+				},
+
+				// Configuration des traductions.
+				language: {
+					default: router.locale ?? "en",
+					translations: {
+						en: "./locales/en/common.json",
+						fr: "./locales/fr/common.json"
+					}
+				},
+			}
+		);
+	}, [ router.basePath, router.locale ] );
+
 	// Affichage du rendu HTML du composant.
 	return (
 		<>
-			{/* Utilisation du contexte de thème. */}
+			{/* Contexte de basculement des thèmes */}
 			<ThemeProvider>
 				{/* Affichage de l'en-tête du site */}
 				{!shouldHide && <Header />}
