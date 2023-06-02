078f387a62658917529d2e5a63ebe27733fa5696
Florian Trayon <git@florian-trayon.fr>
2023-06-02 20:34:00 +0200

    Removed (for now) translations through `next-i18n`

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const basePath = new URL( process.env.NEXT_PUBLIC_URL ?? "" ).pathname;
 /**
  * @type {import("next").NextConfig}
  */
-const { i18n } = require( "./next-i18next.config" );
 const withPWA = require( "next-pwa" )( {
 	dest: "public",
 	disable: process.env.NODE_ENV === "development",
@@ -22,7 +21,6 @@ const withPWA = require( "next-pwa" )( {
 
 
 module.exports = withPWA( {
-	i18n,
 	basePath: basePath === "/" ? "" : ( basePath.endsWith( "/" ) ? basePath.slice( 0, -1 ) : basePath ),
 	poweredByHeader: false,
 	reactStrictMode: true,
