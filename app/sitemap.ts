7a2b823b84e136bf5635a49712575b4bb8dd5139
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-06-02 10:54:39 +0200

    Improved sitemap file now generated by NextJS built-in solution

---
+++ app/sitemap.ts
@@ -0,0 +1,15 @@
+//
+// Route vers le fichier sitemap du site.
+//  Source : https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap
+//
+import { MetadataRoute } from "next";
+
+export default function Sitemap(): MetadataRoute.Sitemap
+{
+	return [
+		{
+			url: process.env.NEXT_PUBLIC_URL ?? "",
+			lastModified: new Date()
+		}
+	];
+}
