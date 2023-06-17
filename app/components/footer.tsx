c7f5b7fd313d78fbe582d6b0245e105799651350
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-06-17 17:36:21 +0200

    Added a redirection link in the footer to legacy pages

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,10 @@
 //
 // Composant du pied de page du site.
 //
+import Link from "next/link";
 import { Trans } from "react-i18next/TransWithoutContext";
+import { faPhp } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer()
 {
@@ -28,6 +31,18 @@ export default function Footer()
 					components={{
 						a1: <a href="https://policies.google.com/privacy">...</a>,
 						a2: <a href="https://policies.google.com/terms">...</a>
+					}}
+				/>
+			</small>
+
+			{/* Lien vers l'ancienne version du site */}
+			<small>
+				<FontAwesomeIcon icon={faPhp} />
+
+				<Trans
+					i18nKey="pages.index.footer_legacy"
+					components={{
+						a: <Link href="/legacy" />
 					}}
 				/>
 			</small>
