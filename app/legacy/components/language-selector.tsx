ab47c0dca8c8b04ec3ed686d65a97778d6abe319
Florian Trayon <git@florian-trayon.fr>
2023-06-17 15:44:57 +0200

    Fixed language switching mechanism in legacy website

--- app/legacy/components/language-selector.tsx
+++ app/legacy/components/language-selector.tsx
@@ -2,15 +2,27 @@
 // Composant du sélecteur de langue de l'ancienne version du site.
 //
 
+"use client";
+
 import { lazy } from "react";
-import { useTranslation } from "@/utilities/ServerTranslations";
+import { useTranslation } from "@/utilities/ClientTranslations";
+import { useRouter, usePathname } from "next/navigation";
 
 const ScrollTop = lazy( () => import( "./scroll-top" ) );
 
-export default async function LanguageSelector()
+export default function LanguageSelector()
 {
 	// Déclaration des constantes.
-	const { t } = await useTranslation();
+	const { t } = useTranslation();
+	const router = useRouter();
+	const pathname = usePathname();
+
+	// Change la langue actuellement sur le site.
+	const switchLanguage = ( language: string ) =>
+	{
+		router.replace( `${ pathname }?language=${ language }` );
+		router.refresh();
+	};
 
 	// Affichage du rendu HTML du composant.
 	return (
@@ -18,26 +30,29 @@ export default async function LanguageSelector()
 			{/* Sélection de la langue */}
 			<ul id="flags">
 				<li>
-					<button type="button">
+					<button type="button" onClick={() => switchLanguage( "fr" )}>
 						<i className="fi fi-fr" />
 						<span>{t( "pages.legacy.language.fr" )}</span>
 					</button>
 				</li>
+
 				<li>
-					<button type="button">
+					<button type="button" onClick={() => switchLanguage( "en" )}>
 						<i className="fi fi-gb" />
 						<span>{t( "pages.legacy.language.en" )}</span>
 					</button>
 				</li>
+
 				<li>
 
-					<button type="button">
+					<button type="button" onClick={() => switchLanguage( "es" )}>
 						<i className="fi fi-es" />
 						<span>{t( "pages.legacy.language.es" )}</span>
 					</button>
 				</li>
+
 				<li>
-					<button type="button">
+					<button type="button" onClick={() => switchLanguage( "jp" )}>
 						<i className="fi fi-jp" />
 						<span>{t( "pages.legacy.language.jp" )}</span>
 					</button>
--- utilities/NextRouter.ts
+++ utilities/NextRouter.ts
@@ -14,3 +14,20 @@ export const getBasePath = ( trailingSlash?: boolean ) =>
 
 	return basePath.endsWith( "/" ) ? basePath.slice( 0, -1 ) : basePath;
 };
+
+//
+// Permet de récupérer la langue sélectionné par l'utilisateur.
+//  Note : ceci n'est plus implémenté dans le routeur de Next.js.
+//
+export const getLanguage = ( headers: Headers ) =>
+{
+	// On récupère d'abord la chaîne de requête.
+	const queryString = decodeURIComponent( headers.get( "X-Invoke-Query" ) ?? "" );
+
+	// On traite la chaîne de requête pour récupérer les paramètres.
+	const parameters = JSON.parse( queryString.length > 0 ? queryString : "{}" ) as { language?: string; };
+
+	// On retourne alors la langue sélectionnée par l'utilisateur
+	//  ou la langue par défaut du navigateur.
+	return parameters.language ?? headers.get( "Accept-Language" )?.substring( 0, 2 ) ?? "en";
+};
