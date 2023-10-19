46d89e5aad60be118c11591a39af71ada968754f
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-10-19 12:44:50 +0200

    Improved toggle mechanism between color themes

---
+++ app/components/theme-provider.tsx
@@ -0,0 +1,195 @@
+//
+// Composant de basculement entre les thèmes clair et sombre.
+//  Source : https://github.com/pacocoursey/next-themes/blob/cd67bfa20ef6ea78a814d65625c530baae4075ef/packages/next-themes/src/index.tsx
+//
+
+"use client";
+
+import { useMemo,
+	useState,
+	useEffect,
+	useContext,
+	useCallback,
+	createContext,
+	type ReactNode } from "react";
+
+// Déclaration des types.
+interface UseThemeProps {
+	theme?: string;
+	setTheme: ( value: string ) => void;
+}
+
+interface ThemeProviderProps {
+	children?: ReactNode;
+}
+
+// Déclaration des constantes globales.
+const storageKey = "NEXT_THEME";
+const ThemeContext = createContext<UseThemeProps | undefined>( undefined );
+const defaultContext: UseThemeProps = { theme: "light",
+	setTheme: () =>
+	{} };
+
+// Définition du fonctionnement du composant.
+function Theme( { children = null }: ThemeProviderProps )
+{
+	// Récupération du thème choisi par l'utilisateur.
+	const getCurrentTheme = ( key: string ) =>
+	{
+		if ( typeof window === "undefined" )
+		{
+			// Valeur par défaut pour le serveur.
+			return "light";
+		}
+
+		return sessionStorage.getItem( key ) ?? "light";
+	};
+
+	// Récupération du thème du navigateur.
+	const getSystemTheme = ( event?: MediaQueryList | MediaQueryListEvent ) =>
+	{
+		if ( typeof window === "undefined" )
+		{
+			// Valeur par défaut pour le serveur.
+			return "light";
+		}
+
+		// Détermination du thème par défaut.
+		const media = event
+			? null
+			: window.matchMedia( "(prefers-color-scheme: dark)" );
+
+		return media?.matches ? "dark" : "light";
+	};
+
+	// Déclaration des variables d'état.
+	const [ theme, setTheme ] = useState( () => getCurrentTheme( storageKey ) );
+
+	// Application du nouveau thème.
+	const applyTheme = useCallback( ( value: string ) =>
+	{
+		// On vérifie d'abord si la valeur choisie est valide.
+		if ( value !== "light" && value !== "dark" )
+		{
+			return;
+		}
+
+		// On définit ensuite cette même valeur avant de
+		//  la sauvegarder dans le stockage de session.
+		setTheme( value );
+
+		sessionStorage.setItem( storageKey, value );
+
+		// On applique enfin les classes CSS correspondantes
+		//  sur le DOM.
+		const element = document.documentElement;
+		element.classList.remove( "light", "dark", "cc--darkmode" );
+		element.classList.add( value );
+		element.style.colorScheme = value;
+
+		if ( value === "dark" )
+		{
+			// Support pour le thème sombre pour les fenêtres
+			//  de consentement des cookies.
+			element.classList.add( "cc--darkmode" );
+		}
+	}, [] );
+
+	// Application du nouveau thème détecté précédemment.
+	const detectMedia = useCallback(
+		( event: MediaQueryListEvent | MediaQueryList ) =>
+		{
+			applyTheme( getSystemTheme( event ) );
+		},
+		[ applyTheme ]
+	);
+
+	// Application du nouveau thème modifié précédemment.
+	const detectStorage = useCallback(
+		( event: StorageEvent ) =>
+		{
+			if ( event.key !== storageKey )
+			{
+				return;
+			}
+
+			applyTheme( event.newValue ?? theme );
+		},
+		[ applyTheme, theme ]
+	);
+
+	// Détection et mise à jour du thème préférée par
+	//  le navigateur.
+	useEffect( () =>
+	{
+		// On récupère d'abord le thème par défaut.
+		const media = window.matchMedia( "(prefers-color-scheme: dark)" );
+
+		// On exécute enfin la fonction à chaque
+		//  changement du thème par défaut.
+		media.addEventListener( "change", detectMedia );
+
+		return () => media.removeEventListener( "change", detectMedia );
+	}, [ detectMedia ] );
+
+	// Détection des modifications du stockage de session
+	//  pour le thème choisi par l'utilisateur.
+	useEffect( () =>
+	{
+		window.addEventListener( "storage", detectStorage );
+
+		return () => window.removeEventListener( "storage", detectStorage );
+	}, [ detectStorage ] );
+
+	// Affichage du rendu HTML du composant.
+	const value = useMemo(
+		() => ( { theme, setTheme: applyTheme } ),
+		[ theme, applyTheme ]
+	);
+
+	return (
+		<ThemeContext.Provider value={value}>
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `
+						const theme = sessionStorage.getItem("${ storageKey }");
+						const element = document.documentElement;
+						const classes = element.classList;
+
+						classes.remove("light", "dark");
+
+						if (theme === "light" || theme === "dark")
+						{
+							// Application du thème choisi par l'utilisateur.
+							classes.add(theme)
+
+							element.style.colorScheme = theme;
+						}
+						else
+						{
+							// Application du thème préféré par le navigateur.
+							const target = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+
+							classes.add(target);
+
+							element.style.colorScheme = target;
+
+							sessionStorage.setItem("${ storageKey }", target);
+						}
+					`
+				}}
+			/>
+
+			{children}
+		</ThemeContext.Provider>
+	);
+}
+
+// Exportation du context du composant.
+export const useTheme = () => useContext( ThemeContext ) ?? defaultContext;
+
+// Exportation du composant.
+export function ThemeProvider( { children = null }: ThemeProviderProps )
+{
+	return <Theme>{children}</Theme>;
+}
