a91274b918c79904b6e8566b41a708dd8e243806
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-10-20 10:22:11 +0200

    Added fallback translations for legacy page languages

--- utilities/i18n.ts
+++ utilities/i18n.ts
@@ -2,9 +2,14 @@
 // Fichier de configuration de la bibliothèque « next-intl ».
 //  Source : https://next-intl-docs.vercel.app/docs/getting-started/app-router-server-components
 //
+import deepmerge from "deepmerge";
 import { getRequestConfig } from "next-intl/server";
+import type { AbstractIntlMessages } from "next-intl";
 
 export default getRequestConfig( async ( { locale } ) => ( {
 	timeZone: "Europe/Paris",
-	messages: ( await import( `../public/locales/${ locale }.json` ) ).default
+	messages: deepmerge(
+		( await import( "../public/locales/en.json" ) ).default,
+		( await import( `../public/locales/${ locale }.json` ) ).default
+	) as unknown as AbstractIntlMessages
 } ) );
