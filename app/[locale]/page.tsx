85ddf1844c99865fca3e4afc3aa343a8bc9d37b3
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-01-08 17:12:21 +0100

    Added `Raven Framework` to project list in data files

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -28,6 +28,7 @@ import Assignment from "@/images/assignment.png";
 import FileStorage from "@/images/filestorage.png";
 import SourceConsole from "@/images/sourceconsole.png";
 import MangaParadise from "@/images/mangaparadise.png";
+import RavenFramework from "@/images/ravenframework.png";
 import DigitalIdentity from "@/images/digitalidentity.png";
 import SteamDownloader from "@/images/steamdownloader.png";
 import FacepunchMonitor from "@/images/facepunchmonitor.png";
@@ -56,7 +57,7 @@ const getImage = ( name: string ) =>
 {
 	switch ( name )
 	{
-		// Domego.
+		// Genio.
 		case "genio":
 			return Genio;
 
@@ -87,6 +88,10 @@ const getImage = ( name: string ) =>
 		// Manga Paradise.
 		case "mangaparadise":
 			return MangaParadise;
+
+		// Raven Framework.
+		case "ravenframework":
+			return RavenFramework;
 
 		// Digital Identity.
 		case "digitalidentity":
