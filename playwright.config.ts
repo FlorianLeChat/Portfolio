0944d8bca5801eda70f36195fa6f796d598ba33b
Florian Trayon <git@florian-trayon.fr>
2024-04-03 21:11:13 +0200

    Added PlayWright tests on additional browsers in GitHub Actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,25 @@ export default defineConfig( {
 		reuseExistingServer: !process.env.CI
 	},
 	projects: [
-		{ name: "Google Chrome", use: { ...devices[ "Desktop Chrome" ] } },
-		{ name: "Microsoft Edge", use: { ...devices[ "Desktop Edge" ] } },
-		{ name: "Mobile Chrome", use: { ...devices[ "Pixel 5" ] } }
+		{
+			name: "chromium",
+			use: { ...devices[ "Desktop Chrome" ] }
+		},
+		{
+			name: "firefox",
+			use: { ...devices[ "Desktop Firefox" ] }
+		},
+		{
+			name: "webkit",
+			use: { ...devices[ "Desktop Safari" ] }
+		},
+		{
+			name: "Mobile Chrome",
+			use: { ...devices[ "Pixel 5" ] }
+		},
+		{
+			name: "Mobile Safari",
+			use: { ...devices[ "iPhone 12" ] }
+		}
 	]
 } as PlaywrightTestConfig );
