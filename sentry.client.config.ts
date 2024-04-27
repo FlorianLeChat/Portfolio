c5e9c27fa0cb1220bc411135eee0501f337dbfd5
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-04-27 20:36:11 +0200

    Reduced Sentry trace sampling rate in production environment

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,15 +1,15 @@
-import { init, Replay } from "@sentry/nextjs";
+import { init, replayIntegration } from "@sentry/nextjs";
 
 init( {
 	dsn: process.env.SENTRY_DSN,
 	debug: false,
 	integrations: [
-		new Replay( {
+		replayIntegration( {
 			maskAllText: true,
 			blockAllMedia: true
 		} )
 	],
-	tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 0.1,
+	tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 0.01,
 	replaysOnErrorSampleRate: 1.0,
 	replaysSessionSampleRate: 0.1
 } );
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,5 +3,5 @@ import { init } from "@sentry/nextjs";
 init( {
 	dsn: process.env.SENTRY_DSN,
 	debug: false,
-	tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 0.1
+	tracesSampleRate: process.env.NODE_ENV === "development" ? 1.0 : 0.01
 } );
