b1d6f4202a8062c3e8b2ce2c90fad0181187c51c
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-06-08 18:51:04 +0200

    Removed real-time website monitoring support by Sentry

--- next.config.js
+++ next.config.js
@@ -1,24 +1,17 @@
 // @ts-check
+const createNextIntlPlugin = require( "next-intl/plugin" );
+const withNextIntl = createNextIntlPlugin( "./utilities/i18n.ts" );
 
 /**
  * @type {import("next").NextConfig}
  */
-const { withSentryConfig } = require( "@sentry/nextjs" );
-const withNextIntl = require( "next-intl/plugin" )( "./utilities/i18n.ts" );
-
-const nextConfig = withNextIntl( {
+module.exports = withNextIntl( {
 	poweredByHeader: false,
 	experimental: {
 		// https://github.com/vercel/next.js/discussions/46987#discussioncomment-8464812
 		serverComponentsExternalPackages: ["pino", "pino-pretty"]
 	},
 	basePath: "",
-	sentry: {
-		tunnelRoute: "/monitoring",
-		disableLogger: true,
-		hideSourceMaps: true,
-		widenClientFileUpload: true
-	},
 	async redirects()
 	{
 		return [
@@ -31,14 +24,3 @@ const nextConfig = withNextIntl( {
 		];
 	}
 } );
-
-const sentryConfig = {
-	org: process.env.SENTRY_ORG,
-	silent: true,
-	project: process.env.SENTRY_PROJECT,
-	authToken: process.env.SENTRY_AUTH_TOKEN
-};
-
-module.exports = process.env.SENTRY_ENABLED === "true"
-	? withSentryConfig( nextConfig, sentryConfig )
-	: nextConfig;
