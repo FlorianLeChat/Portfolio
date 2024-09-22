f42ce9c786478d4101267bfd31ebab8605964ed8
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-09-22 17:20:45 +0200

    Added Pino to manage debug messages from npm dependencies

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const withNextIntl = createNextIntlPlugin( "./utilities/i18n.ts" );
  */
 module.exports = withNextIntl( {
 	poweredByHeader: false,
+	experimental: {
+		// https://github.com/vercel/next.js/discussions/46987#discussioncomment-8464812
+		serverComponentsExternalPackages: ["pino", "pino-pretty"]
+	},
 	basePath: "",
 	async redirects()
 	{
