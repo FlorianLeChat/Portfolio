4b19a39180ec41784fca8972eac85c1e9a66d11f
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-05-10 20:21:23 +0200

    Fixed automatic e2e PlayWright tests following recent changes

--- tests/e2e/navigation.spec.ts
+++ tests/e2e/navigation.spec.ts
@@ -135,7 +135,7 @@ test( "Disponibilité du C.V", async ( { page, context } ) =>
 	await drivePage.waitForLoadState();
 
 	// Vérification du titre de la nouvelle page.
-	await expect( drivePage ).toHaveTitle( /CV - Florian Trayon.pdf/ );
+	await expect( drivePage ).toHaveTitle( "Florian Trayon - Curriculum Vitae" );
 } );
 
 //
