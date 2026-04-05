b3d57ab6c1778be84de5cb0ac708c9c2af58fb76
Florian Trayon <git@florian-trayon.fr>
2026-04-05 11:10:58 +0200

    refactor(next): revert all recent changes related to NextJS metadata generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,15 @@
+import { fetchMetadata } from "@/utilities/metadata";
+
 export default async function Sitemap()
 {
+    const baseUrl = new URL( ( await fetchMetadata() )?.metadataBase ?? "" );
+    const pathname = baseUrl.pathname.endsWith( "/" )
+        ? baseUrl.pathname
+        : `${ baseUrl.pathname }/`;
+
     return [
         {
-            url: "/",
+            url: baseUrl + pathname,
             lastModified: new Date()
         }
     ];
--- utilities/metadata.ts
+++ utilities/metadata.ts
@@ -38,7 +38,9 @@ export async function fetchMetadata(): Promise<Metadata & { source: string }>
 
     const banner = `https://opengraph.githubassets.com/${ commits.sha }/${ repository.full_name }`;
     const title = `${ author.name } - ${ repository.name }`;
-    const url = repository.homepage;
+    const url = process.env.NEXT_PUBLIC_ENV === "production"
+        ? repository.homepage
+        : `http://localhost:${ process.env.PORT ?? 3000 }`;
 
     const metadata = {
         title,
@@ -50,34 +52,34 @@ export async function fetchMetadata(): Promise<Metadata & { source: string }>
         icons: {
             icon: [
                 {
-                    url: `${ url }/assets/favicons/16x16.webp`,
+                    url: "assets/favicons/16x16.webp",
                     type: "image/webp",
                     sizes: "16x16"
                 },
                 {
-                    url: `${ url }/assets/favicons/32x32.webp`,
+                    url: "assets/favicons/32x32.webp",
                     type: "image/webp",
                     sizes: "32x32"
                 },
                 {
-                    url: `${ url }/assets/favicons/48x48.webp`,
+                    url: "assets/favicons/48x48.webp",
                     type: "image/webp",
                     sizes: "48x48"
                 },
                 {
-                    url: `${ url }/assets/favicons/192x192.webp`,
+                    url: "assets/favicons/192x192.webp",
                     type: "image/webp",
                     sizes: "192x192"
                 },
                 {
-                    url: `${ url }/assets/favicons/512x512.webp`,
+                    url: "assets/favicons/512x512.webp",
                     type: "image/webp",
                     sizes: "512x512"
                 }
             ],
             apple: [
                 {
-                    url: `${ url }/assets/favicons/180x180.webp`,
+                    url: "assets/favicons/180x180.webp",
                     type: "image/webp",
                     sizes: "180x180"
                 }
